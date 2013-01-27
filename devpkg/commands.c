297b270e096c0869a744c799d554dd468e9b53f6
Paul Forey <paul.forey@gmail.com>
2013-01-27 11:58:47 +0000

    localness of apr headers for Pandora

--- devpkg/commands.c
+++ devpkg/commands.c
@@ -7,7 +7,8 @@
 #include "shell.h"
 #include "dbg.h"
 
-int Command_depends(apr_pool_t* p, const char* path)
+int Command_depends(apr_pool_t* p,
+		const char* path)
 {
 	FILE* in = NULL;
 	bstring line = NULL;
@@ -31,7 +32,9 @@ error:
 	return -1;
 }
 
-int Command_fetch(apr_pool_t* p, const char* url, int feth_only)
+int Command_fetch(apr_pool_t* p,
+		const char* url,
+		int			fetch_only)
 {
 	apr_uri_t info = {.port = 0};
 	int rc = 0;
@@ -62,5 +65,100 @@ int Command_fetch(apr_pool_t* p, const char* url, int feth_only)
 		// needs to be done, so we return:
 		return 0;
 	} else if(apr_fnmatch(TAR_GZ_PAT, info.path, 0) == APR_SUCCESS) {
+		if(info.scheme) {
+			rc = Shell_exec(CURL_SH,
+					"URL", url,
+					"TARGET", TAR_GZ_SRC, NULL);
+			check(rc == 0, "Failed to curl source: %s", url);
+		}
+		rv = apr_dir_make_recursive(BUILD_DIR,
+				APR_UREAD | APR_UWRITE | APR_UEXECUTE, p);
+		check(rv == APR_SUCCESS, "Failed to make directory: %s",
+				BUILD_DIR);
+		rc = Shell_exec(TAR_SH, "FILE", TAR_GZ_SRC, NULL);
+		check(rc == 0, "Failed to untar %s", TAR_GZ_SRC);
+	} else if(apr_fnmatch(TAR_BZ2_PAT, info.path, 0) == APR_SUCCESS) {
+		if(info.scheme) {
+			rc = Shell_exec(CURL_SH,
+					"URL", url,
+					"TARGET", TAR_BZ2_SRC, NULL);
+			check(rc == 0, "Failed to curl source: %s", url);
+		}
+		rv = apr_dir_make_recursive(BUILD_DIR,
+				APR_UREAD | APR_UWRITE | APR_UEXECUTE, p);
+		check(rv == APR_SUCCESS, "Failed to make directory: %s",
+				BUILD_DIR);
+		rc = Shell_exec(TAR_SH, "FILE", TAR_BZ2_SRC, NULL);
+		check(rc == 0, "Failed to untar %s", TAR_BZ2_SRC);
+	} else {
+		sentinel("Don't know how to handle %s", url);
+	}
+	// Install needs to run if we're here.
+	return 1;
+error:
+	return -1;
+}
+int Command_build(apr_pool_t* p,
+		const char* url,
+		const char* configure_opts,
+		const char* make_opts,
+		const char* install_opts)
+{
+	int rc = 0;
+	check(access(BUILD_DIR, X_OK | R_OK | W_OK) == 0,
+		"Build directory doesn't exist: %s", BUILD_DIR);
 
+	// Actually do an install:
+	if(access(CONFIG_SCRIPT, X_OK) == 0) {
+		log_info("Has a configure script, running it.");
+		rc = Shell_exec(CONFIGURE_SH, "OPTS", configure_opts, NULL);
+		check(rc == 0, "Failed to configure.");
+	}
+	rc = Shell_exec(MAKE_SH, "OPTS", make_opts, NULL);
+	check(rc == 0, "Failed to build.");
+	rc = Shell_exec(INSTALL_SH,
+			"TARGET", install_opts ? install_opts : "install", NULL);
+	check(rc == 0, "Failed to install.");
+	rc = Shell_exec(CLEANUP_SH, NULL);
+	check(rc == 0, "Failed to clean up after build.");
+	rc = DB_update(url);
+	check(rc == 0, "Failed to add this package to the database.");
+	return 0;
+error:
+	return -1;
+}
+
+int Command_install(apr_pool_t* p,
+		const char* url,
+		const char* configure_opts,
+		const char* make_opts,
+		const char* install_opts)
+{
+	int rc = 0;
+	check(Shell_exec(CLEANUP_SH, NULL) == 0,
+			"Failed to clean up before build.");
+	rc = DB_find(url);
+	check(rc != -1, "Error checking the install database.");
+	if(rc == 1) {
+		log_info("Package %s already installed.", url);
+		return 0;
+	}
+	rc = Command_fetch(p, url, 0);
+	if(rc == 1) {
+		rc = Command_build(p, url, configure_opts,
+				make_opts, install_opts);
+		check(rc == 0, "Failed to build: %s", url);
+	} else if(rc == 0) {
+		// No install needed:
+		log_info("Depends successfully installed: %s", url);
+	} else {
+		// Had an error:
+		sentinel("Install failed: %s", url);
+	}
+	Shell_exec(CLEANUP_SH, NULL);
+	return 0;
+error:
+	Shell_exec(CLEANUP_SH, NULL);
+	return -1;
+}
 
