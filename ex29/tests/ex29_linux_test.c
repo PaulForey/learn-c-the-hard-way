38b580663172efeceb4a829f661354705478fceb
Paul Forey <paul.forey@gmail.com>
2013-03-04 17:28:10 +0000

    more improvements to ex29

--- ex29/tests/ex29_linux_test.c
+++ ex29/tests/ex29_linux_test.c
@@ -4,25 +4,23 @@
 
 #define LIB_FILE "build/libex29.so"
 
-typedef int (*lib_function)(const char* data);
+typedef int (*lib_function)(const char* data, int length);
 
 int test_print_a_message(void* lib)
 {
     int rc = 0;
+	char msg[] = "Print this message!";
     lib_function func = dlsym(lib, "print_a_message");
     check(func != NULL, "Did not find print_a_message function in the library %s", LIB_FILE);
 
-    debug("Zero");
-    rc = func("Print this message!");
-    debug("One");
+	debug("%d", sizeof(msg));
+    rc = func(msg, sizeof(msg));
     check(rc == 0, "Failed to print message.");
-    debug("Two");
 
     char broken_string[4] = {'a', 'b', 'c', 'd'};
 
-    //rc = func(broken_string);
+    rc = func(broken_string, 4);
     check(rc == 0, "Failed to print message from broken string.");
-    debug("three");
 
     return 0;
 
@@ -33,16 +31,17 @@ error:
 int test_uppercase(void* lib)
 {
     int rc = 0;
+	char msg[] = "What is all this about?";
 
     lib_function func = dlsym(lib, "uppercase");
     check(func != NULL, "Did not find uppercase function in the library %s", LIB_FILE);
 
-    rc = func("What is all this about?");
+    rc = func(msg, sizeof(msg));
     check(rc == 0, "Failed to change case.");
 
     char broken_string[4] = {'a', 'b', 'c', 'd'};
 
-    //rc = func(broken_string);
+    rc = func(broken_string, 4);
     check(rc == 0, "Failed to change case of broken string.");
 
     return 0;
@@ -54,16 +53,18 @@ error:
 int test_lowercase(void* lib)
 {
     int rc = 0;
+	char msg[] = "What? What?!? Where are the CATS gone?";
 
     lib_function func = dlsym(lib, "lowercase");
     check(func != NULL, "Did not find lowercase function in the library %s", LIB_FILE);
 
-    func("What? What?!? Where are the CATS gone?");
+    rc = func(msg, sizeof(msg));
     check(rc == 0, "Failed to lower case.");
+	debug("rc = %d", rc);
 
     char broken_string[4] = {'a', 'b', 'c', 'd'};
 
-    //func(broken_string);
+    func(broken_string, 4);
     check(rc == 0, "Failed to lower case of broken string.");
 
     return 0;
@@ -75,27 +76,26 @@ error:
 int test_fail_on_purpose(void* lib)
 {
     int rc = 0;
+	char msg[] = "It just doesn't matter.";
 
     lib_function func = dlsym(lib, "fail_on_purpose");
     check(func != NULL, "Did not find fail_on_purpose function in the library %s", LIB_FILE);
 
-    func("It just doesn't matter!");
+    func(msg, sizeof(msg));
     check(rc == 0, "fail_on_purpose failed! Who have thunk it?");
 
-    return 0;
+    return 1;
 
 error:
-    return 1;
+    return 0;
 }
 
 int main()
 {
     int rc = 0;
 
-    debug("Hello!");
 
     void* lib = dlopen(LIB_FILE, RTLD_NOW);
-    debug("Error?");
     check(lib != NULL, "Failed to open the library %s: %s", LIB_FILE, dlerror());
 
     /// Start some tests:
@@ -107,12 +107,10 @@ int main()
     rc = test_lowercase(lib);
     check(rc == 0, "lowercase failed it's test.");
     rc = test_fail_on_purpose(lib);
-    check(rc == 0, "fail_on_purpose failed it's test.");
+    check(rc == 1, "fail_on_purpose didn't fail. This is a failure.");
 
     rc = dlclose(lib);
     check(rc == 0, "Failed to close %s: %s", LIB_FILE, dlerror());
-
-    debug("Goodbye!");
 
     return 0;
 
--- ex29/tests/runtests.sh
+++ ex29/tests/runtests.sh
@@ -1,20 +1,24 @@
-echo "Clearing test log (old log renamed to 'log.old')."
 mv tests/tests.log tests/tests.log.old
 touch tests/tests.log
+echo "Cleared test log (previous log renamed to 'tests.log.old')."
 
 echo "Running unit tests:"
 
-for i in tests/*_test
+for i in tests/$TESTPAT
 do
     if test -f $i
     then
-        if $VALGRIND ./$i 2>> tests/tests.log
+        if $VALGRIND ./$i &> tests/tests.log
         then
-            echo $i PASS
+            echo "$i PASS. Here's tests/tests.log:"
+            echo "------------"
+            cat tests/tests.log
+            echo "------------"
         else
             echo "ERROR in $i. Here's tests/tests.log:"
             echo "------------"
-            tail tests/tests.log
+            cat tests/tests.log
+            echo "------------"
             exit 1
         fi
     fi
