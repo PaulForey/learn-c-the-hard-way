e704df8a374326253febfebb488cd82733f25cc4
Paul Forey <paul.forey@gmail.com>
2013-03-18 10:45:23 +0000

    started lcthw library

---
+++ lcthw/test/runtests.sh
@@ -0,0 +1,34 @@
+mv test/tests.log test/tests.log.old
+touch test/tests.log
+
+echo "Running unit tests:"
+
+ERRORS=0
+
+for i in test/*_test
+do
+    if test -f $i
+    then
+		echo "----------------------"
+		echo "RUNNING: $i"
+        if $VALGRIND ./$i 2>> test/tests.log
+        then
+            echo "PASSED: $i"
+        else
+            echo -e "\tERROR in $i."
+			((ERRORS++))
+        fi
+    fi
+done
+
+echo "----------------------"
+if test $ERRORS -gt 0
+then
+	echo -e "\tERRORS FOUND, here's tests/tests.log:"
+	echo "=============================================="
+	cat test/tests.log
+	echo "=============================================="
+	exit 1
+else
+	echo "All tests passed! You win a hot dog!"
+fi
