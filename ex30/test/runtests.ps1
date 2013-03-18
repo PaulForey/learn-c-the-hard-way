a2f6fd3e360d95e97b862144810bba3cca783076
Paul Forey <paul.forey@gmail.com>
2013-03-18 19:06:51 +0000

    ex30 working fine on Windows and Linux. Having problems with runtests.sh, though.

--- ex30/test/runtests.ps1
+++ ex30/test/runtests.ps1
@@ -1,25 +1,41 @@
 Write-Host "Clearing test log (old log renamed to 'log.old')."
-Copy-Item tests/tests.log tests/tests.log.old
-Clear-Content tests/tests.log
+Copy-Item test/tests.log test/tests.log.old
+Clear-Content test/tests.log
 
 Write-Host "Running Unit Tests:"
 
-foreach($i in Get-ChildItem tests -filter "*_win_test.exe") {
-        echo "$ENV:DRMEMORY./tests/$i"
-        $result = & "$ENV:DRMEMORY./tests/$i" 2> tests/tests.log
-        #$result = & "drmemory -batch ./tests/$i" 2> tests/tests.log
-        #$result = $i > tests/tests.log
-        if($result) {
-                Write-Host $i PASS
-                Write-Host "Here's tests/tests.log`:"
-                Get-Content tests/tests.log
+$errors = 0
+
+foreach($i in Get-ChildItem test -filter "*_test.exe") {
+        if($ENV:DRMEMORY) {
+                echo "$ENV:DRMEMORY./test/$i"
+                $result = & "$ENV:DRMEMORY./test/$i" 2> test/tests.log
         }
         else {
-                Write-Host "ERROR in test $i. Here's tests/tests.log`:"
-                Write-Host "------"
-                Get-Content tests/tests.log
-                exit 1
+                echo ./test/$i
+                $result = & "./test/$i" > test/tests.log
+        }
+        #$result = & "drmemory -batch ./test/$i" 2> test/tests.log
+        #$result = $i > test/tests.log
+        if($result) {
+                Write-Host $i PASS
+        }
+        else {
+                Write-Host "ERROR in test $i"
+                $errors++
         }
 }
 
 Write-Host ""
+if($errors -gt 0) {
+        Write-Host "$errors tests failed. Here's test/tests.log`:"
+        Write-Host "==========================="
+        Get-Content test/tests.log
+        Write-Host "==========================="
+        exit 1
+}
+else {
+        Write-Host "All tests passed! You win a hot dog!"
+        Get-Content test/tests.log
+}
+
