f28e019fc298a1d13f8fa3753b169b0d77665778
Paul Forey <paul.forey@gmail.com>
2014-11-24 00:03:51 +0000

    hashmap_algos_test now tests default hash; removed results text file from source control

--- liblcthw/test/hashmap_algos_test.c
+++ liblcthw/test/hashmap_algos_test.c
@@ -8,6 +8,19 @@ struct tagbstring test1 = bsStatic("test data 1");
 struct tagbstring test2 = bsStatic("test data 2");
 struct tagbstring test3 = bsStatic("xest data 3"); //may be typo
 
+char* test_default()
+{
+    uint32_t hash = Hashmap_default_hash(&test1);
+    mu_assert(hash != 0, "Bad hash.");
+    
+    hash = Hashmap_default_hash(&test2);
+    mu_assert(hash != 0, "Bad hash.");
+
+    hash = Hashmap_default_hash(&test3);
+    mu_assert(hash != 0, "Bad hash.");
+    
+    return NULL;
+}
 char* test_fnvla()
 {
     uint32_t hash = Hashmap_fnv1a_hash(&test1);
@@ -50,10 +63,24 @@ char* test_djb()
     return NULL;
 }
 
+char* test_pfa()
+{
+    uint32_t hash = Hashmap_pfa_hash(&test1);
+    mu_assert(hash != 0, "Bad hash.");
+    
+    hash = Hashmap_pfa_hash(&test2);
+    mu_assert(hash != 0, "Bad hash.");
+    
+    hash = Hashmap_pfa_hash(&test3);
+    mu_assert(hash != 0, "Bad hash.");
+    
+    return NULL;
+}
+
 #define BUCKETS 100
 #define BUFFER_LEN 20
-#define NUM_KEYS BUCKETS * 1000
-enum { ALGO_FNV1A, ALGO_ADLER32, ALGO_DJB};
+#define NUM_KEYS BUCKETS * 2000
+enum { ALGO_DEFAULT, ALGO_FNV1A, ALGO_ADLER32, ALGO_DJB, ALGO_PFA};
 
 int gen_keys(DArray* keys, int num_keys)
 {
@@ -102,36 +129,36 @@ void fill_distribution(int* stats, DArray* keys, Hashmap_hash hash_func)
     }
 }
 
+#define NUM_OF_HASHES 5
+
 char* test_distribution()
 {
     int i = 0;
-    int stats[3][BUCKETS] = {{0}};
+    int stats[NUM_OF_HASHES][BUCKETS] = {{0}};
     DArray* keys = DArray_create(sizeof(bstring), NUM_KEYS);
     mu_assert(keys != NULL, "DArray not created properly.");
     
     mu_assert(gen_keys(keys, NUM_KEYS) == 0, "Failed to generate random keys.");
     
+    fill_distribution(stats[ALGO_DEFAULT], keys, Hashmap_default_hash);
     fill_distribution(stats[ALGO_FNV1A], keys, Hashmap_fnv1a_hash);
     fill_distribution(stats[ALGO_ADLER32], keys, Hashmap_adler32_hash);
     fill_distribution(stats[ALGO_DJB], keys, Hashmap_djb_hash);
+    fill_distribution(stats[ALGO_PFA], keys, Hashmap_pfa_hash);
     
     FILE* result_table = fopen("test/hashmap_algos_test_results.txt", "w");
-    fprintf(result_table, "FNV\tA32\tDJB\n");
-    fprintf(stderr, "FNV\tA32\tDJB\n");
+    fprintf(result_table, "DEF\tFNV\tA32\tDJB\tPFA\n");
     
     for(i = 0; i < BUCKETS; i++) {
-        fprintf(stderr, "%d\t%d\t%d\n",
+        fprintf(result_table, "%d\t%d\t%d\t%d\t%d\n",
+                stats[ALGO_DEFAULT][i],
                 stats[ALGO_FNV1A][i],
                 stats[ALGO_ADLER32][i],
-                stats[ALGO_DJB][i]);
-        fprintf(result_table, "%d\t%d\t%d\n",
-                stats[ALGO_FNV1A][i],
-                stats[ALGO_ADLER32][i],
-                stats[ALGO_DJB][i]);
+                stats[ALGO_DJB][i],
+                stats[ALGO_PFA][i]);
     }
     
     fclose(result_table);
-    
     
     destroy_keys(keys);
     
@@ -144,6 +171,7 @@ char* all_tests()
     mu_run_test(test_fnvla);
     mu_run_test(test_adler32);
     mu_run_test(test_djb);
+    mu_run_test(test_pfa);
     mu_run_test(test_distribution);
     
     return NULL;
